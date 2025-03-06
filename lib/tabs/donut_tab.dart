c725b21360af3a2a47ed9ff091970055de021cc0
Eduardo Ruiz <eduardoyahel@gmail.com>
2025-03-06 12:48:19 -0600

    SliverGridDelegateWithFixedCrossAxisCount

--- lib/tabs/donut_tab.dart
+++ lib/tabs/donut_tab.dart
@@ -5,8 +5,19 @@ class DonutTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Donut Tab"),
-    );
+    return GridView.builder(
+        //prepa 1: como se va organizar
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            //número de columnas
+            crossAxisCount: 4),
+        //cuantos elementos se van a mostrar
+        itemCount: 10, // specify the number of items
+        padding: const EdgeInsets.all(12),
+        //que elemento se construira
+        itemBuilder: (context, index) {
+          return Card(
+            child: Center(child: Text('Item $index')),
+          );
+        });
   }
 }
