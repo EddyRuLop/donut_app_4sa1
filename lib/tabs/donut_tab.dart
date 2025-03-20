ad6dbaa4b382ba5fade32f5e810c0a4a92ef735b
Eduardo Ruiz <eduardoyahel@gmail.com>
2025-03-20 13:55:18 -0600

    Starting DonutTile

--- lib/tabs/donut_tab.dart
+++ lib/tabs/donut_tab.dart
@@ -1,22 +1,36 @@
+import 'package:donut_app_4sa/utils/donut_tile.dart';
 import 'package:flutter/material.dart';
 
 class DonutTab extends StatelessWidget {
-  const DonutTab({super.key});
+  //List of donuts
+  final List donutsOnSale = [
+    // [ donutFlavor, donutPrice, donutColor, imageName ]
+    ["Ice Cream", "36", Colors.blue, "lib/images/icecream_donut.png"],
+    ["Strawberry", "45", Colors.red, "lib/images/strawberry_donut.png"],
+    ["Grape Ape", "84", Colors.purple, "lib/images/grape_donut.png"],
+    ["Choco", "95", Colors.brown, "lib/images/chocolate_donut.png"],
+  ];
+
+  DonutTab({super.key});
 
   @override
   Widget build(BuildContext context) {
     return GridView.builder(
         //prepa 1: como se va organizar
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            //número de columnas
-            crossAxisCount: 4),
+          //número de columnas
+          crossAxisCount: 2,
+        ),
         //cuantos elementos se van a mostrar
-        itemCount: 10, // specify the number of items
+        itemCount: 4, // specify the number of items
         padding: const EdgeInsets.all(12),
         //que elemento se construira
         itemBuilder: (context, index) {
-          return Card(
-            child: Center(child: Text('Item $index')),
+          return DonutTile(
+            donutFlavor: donutsOnSale[index][0],
+            donutPrice: donutsOnSale[index][1],
+            donutColor: donutsOnSale[index][2],
+            imageName: donutsOnSale[index][3],
           );
         });
   }
